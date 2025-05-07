426dd0f870a54d9299e1dd59a2695d13f1e5c7b3
vechnoty <mehonoshin330@gmail.com>
2025-05-07 18:05:43 +0300

    Delete TextAnalysis directory

--- TextAnalysis/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TextAnalysis.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
